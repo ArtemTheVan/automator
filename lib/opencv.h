e9853fa1bc97cc6cf3de4c6df37569a809d6faee
Artem Ivannikov <a.ivannikov@aladdin.ru>
2026-01-07 21:45:03 +0300

    Update opencv.h

--- lib/opencv.h
+++ lib/opencv.h
@@ -29,8 +29,8 @@ extern "C"
     OPENCV_API int opencv_init(void);
     OPENCV_API void opencv_cleanup(void);
     OPENCV_API DetectedRegion detect_system_tray_region_opencv(void);
-    OPENCV_API DetectedRegion find_keyboard_layout_in_region(ScreenRegion region);
     OPENCV_API DetectedRegion *find_text_regions_in_tray(ScreenRegion region, int *region_count);
+    OPENCV_API DetectedRegion find_keyboard_layout_in_region(ScreenRegion region);
 
 #ifdef __cplusplus
 }
